229f30f98e39f104c96bbebb30ecadc6b16556c8
fahricandurucan <fahricandurucan@icloud.com>
2023-04-07 17:05:46 +0300

    toolbar oluşturmaya başlandı

--- app/src/main/java/com/example/techcareerbootcamphomework4/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/com/example/techcareerbootcamphomework4/ui/fragment/AnasayfaFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.techcareerbootcamphomework4.R
+import com.example.techcareerbootcamphomework4.databinding.FragmentAnasayfaBinding
 
 class AnasayfaFragment : Fragment() {
+    private lateinit var binding: FragmentAnasayfaBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_anasayfa, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_anasayfa, container, false)
+        return binding.root
     }
 }
